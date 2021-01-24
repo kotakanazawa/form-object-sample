6e0163e7c6450e450c2b6a6145abea79578012ef
Kota Kanazawa <kota0818@gmail.com>
2021-01-24 15:52:16 +0900

    Remove after-create validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,11 +18,4 @@ class User < ApplicationRecord
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
   validates :password, presence: true
   # validates :terms_of_service, acceptance: true
-  after_create_commit :send_welcome_email
-
-  private
-
-    def send_welcome_email
-      puts "[DONE] メールを送ったぜよ！"
-    end
 end
